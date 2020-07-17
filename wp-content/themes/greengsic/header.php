21d786b12e7ee7a8f6aa7d3e3d5917c227bfcf18
byaction <burakhanefendi@gmail.com>
2020-07-18 02:51:30 +0300

    tema giydirme

---
+++ wp-content/themes/greengsic/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <title>Social Activism</title>
+    <meta charset="utf-8">
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <![endif]-->
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/css/animations.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/css/fonts.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/css/main.css" class="color-switcher-link">
+    <script src="<?=get_template_directory_uri(); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+
+    <script src="<?=get_template_directory_uri(); ?>/js/vendor/html5shiv.min.js"></script>
+    <script src="<?=get_template_directory_uri(); ?>/js/vendor/respond.min.js"></script>
+    <script src="<?=get_template_directory_uri(); ?>/js/vendor/jquery-1.12.4.min.js"></script>
+
+</head>
+
+<body>
