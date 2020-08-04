ea5fa469023cf58a3745a7bada53e32634110318
byaction <burakhanefendi@gmail.com>
2020-08-04 14:47:52 +0300

    development

--- wp-content/themes/greengsic/section/header.php
+++ wp-content/themes/greengsic/section/header.php
@@ -175,10 +175,10 @@
                                 <a href="iletisim">BİZE ULAŞIN</a>
                             </li>
                             <li class="desk-hidden">
-                                <a href="#"><img style="width: 30px;" src="<?=get_template_directory_uri(); ?>/images/turkey.png"></a>
+                                <a href="#">TR</a>
                                 <ul>
-                                    <li><a href="#"><img style="width: 30px;" src="<?=get_template_directory_uri(); ?>/images/uk.png"></a></li>
-                                    <li><a href="#"><img style="width: 30px;" src="<?=get_template_directory_uri(); ?>/images/flag.png"></a></li>
+                                    <li><a href="#">EN</a></li>
+                                    <li><a href="#">FR</a></li>
                                 </ul>
                             </li>
                             <li class="desk-hidden" style="width: 25%; float: left;">
